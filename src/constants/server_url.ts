fba916b89aedb76dcd8535ae6dd66066d04648fb
Sean Luo <shawnluo415@gmail.com>
2021-04-26 23:12:44 -0700

    changed server url

--- src/constants/server_url.ts
+++ src/constants/server_url.ts
@@ -3,6 +3,6 @@ let SERVER_URL = "";
 if (process.env.NODE_ENV != "production") {
   SERVER_URL = "http://localhost:8080";
 } else {
-  SERVER_URL = `https://idsp-mock-server.herokuapp.com`;
+  SERVER_URL = `https://comp2350-week6-sean-luo.herokuapp.com/`;
 }
 export default SERVER_URL;
