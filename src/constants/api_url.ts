041bff0e3c6361457b53d9a9da69d8680ea1f9af
Sean Luo <shawnluo415@gmail.com>
2021-05-01 22:59:45 -0700

    over https

--- src/constants/api_url.ts
+++ src/constants/api_url.ts
@@ -1,6 +1,6 @@
 // const API_URL = "http://671cdc1794ca.ngrok.io/api";
 // const API_URL = "http://localhost:8000/api";
 // J's server ran locally
-const API_URL = "http://idsp.link/api";
+const API_URL = "https://idsp.link/api";
 
 export default API_URL;
