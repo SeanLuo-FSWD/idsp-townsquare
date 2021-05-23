4f99221a310e21a6ec160e3a0a1e14799e9f65da
Johnny Kung <kjohnathan.engi@gmail.com>
2021-05-23 15:18:14 -0700

    changed api url and socket url to https://idsp.link

--- src/constants/api_url.ts
+++ src/constants/api_url.ts
@@ -1,5 +1,5 @@
 // const API_URL = "http://671cdc1794ca.ngrok.io/api";
-const API_URL = "http://34.145.97.81/api";
+const API_URL = "https://idsp.link/api";
 // J's server ran locally
 // const API_URL = "https://idsp.link/api";
 
